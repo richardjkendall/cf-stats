8e3124c70ba367ce1698a12975f33dee8767f98d
Richard Kendall <richard.kendall@gmail.com>
2020-12-24 08:36:44 +1100

    added support for date filtering

--- Dockerfile
+++ Dockerfile
@@ -1,16 +1,19 @@
 FROM ubuntu:20.04
 
+# get basic dependencies
 ENV DEBIAN_FRONTEND=noninteractive 
 RUN apt-get update
-RUN apt-get -y install curl goaccess zip apache2
+RUN apt-get -y install curl webalizer zip apache2 jq
 
+# install aws cli
 RUN curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 RUN unzip awscliv2.zip
 RUN ./aws/install
 
+# get stats script and make executable
 COPY stats.sh /
 RUN chmod +x stats.sh
-#ENTRYPOINT ["/stats.sh"]
+ENTRYPOINT ["/stats.sh"]
 
 # expose apache2 port
 EXPOSE 80
--- stats.sh
+++ stats.sh
@@ -1,14 +1,27 @@
 #!/bin/sh
 
 # get the log files
-mkdir logs
+mkdir -p logs
 cd logs
-# --exclude "*" --include "*2020-12*"
-aws s3 sync --exclude "*" --include "*2020-11-0*" --include "*2020-11-1*" s3://$LOG_BUCKET/$LOG_PREFIX .
+
+# check the config for a list of conditions to include
+# '[{"dateFragment":"2020-10"},{"dateFragment":"2020-11"}]'
+
+for row in $(echo "$FILTER" | jq -r '.[] | @base64'); do
+   _jq() {
+      echo ${row} | base64 --decode | jq -r ${1}
+   }
+   datefrag=$(_jq '.dateFragment')
+   echo "syncing for date fragment: $datefrag"
+   aws s3 cp --exclude "*" --include "*$datefrag*" s3://$LOG_BUCKET/$LOG_PREFIX . --recursive
+done
+
+# go back to the home directory
 cd ..
 
 # build report
-zcat logs/*.gz | goaccess - --log-format=CLOUDFRONT -o index.html
+mkdir -p /var/www/html/stats
+zcat logs/*.gz | webalizer -n $HOSTNAME -F w3c -o /var/www/html/stats/ -
 
-# copy report
-cp index.html /var/www/html
+# run the command
+exec "$@"
